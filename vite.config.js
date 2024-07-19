96bd100bc48a4f57a08bd7b7958ea0295fecd5df
Anastasiia Bilska <nastya.bilskaya@gmail.com>
2024-07-19 21:16:19 +0300

    feat: linters

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+import { resolve, dirname } from 'node:path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+  ],
+  build: {
+    emptyOutDir: true,
+  },
+  define: {
+    'process.env': process.env || {},
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
